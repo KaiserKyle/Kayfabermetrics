25dc1888841d1a8f1a86617079e573b1965abea5
KaiserKyle <KaiserKyle@gmail.com>
2015-11-20 22:06:16 -0800

    Adding the beginning of auto-preview generation

--- python/matchup.py
+++ python/matchup.py
@@ -42,24 +42,38 @@ cur.execute(queryString)
 con.commit()
 currentELO = cur.fetchall();
 
-matches = [[["The Undertaker"], ["Brock Lesnar"]],
-           [["Seth Rollins"],["Kane"]],
-           [["Kevin Owens"],["Ryback"]],
-           [["Nikki Bella"], ["Charlotte"]],
-           [["Dolph Ziggler", "Antonio Cesaro", "Neville"],["Rusev", "Sheamus", "Wade Barrett"]],
-           [["Bubba Ray Dudley","D-Von Dudley"],["Big E. Langston", "Kofi Kingston"]],
-           [["Roman Reigns"],["Bray Wyatt"]]];
+matches = [[["Tyler Breeze"],["Dolph Ziggler"]],
+           [["Charlotte"], ["Paige"]],
+           [["Roman Reigns"], ["Alberto Del Rio"]],
+           [["Dean Ambrose"], ["Kevin Owens"]],
+           [["The Undertaker", "Kane"],["Bray Wyatt", "Luke Harper"]]];
+
+f = open('preview.txt', 'w');
 
 for match in matches:
     teamELOs = list();
     teamQs = list();
+    teamNames = list();
     for team in match:
+        name = "";
+        members = len(team);
+        index = 0;
         teamELO = 0;
         for person in team:
             id = getWrestlerId(person);
             elo = getELO(id);
             print (person + "(" + str(id) + "): " + str(elo));
             teamELO += elo;
+            name += person;
+            if members == 2:
+                if index == 0:
+                    name += " and ";
+            if members > 2:
+                if index < members - 2:
+                    name += ", ";
+                if index == members - 2:
+                    name += ", and ";
+            index += 1;
         #print("Team ELO: " + str(teamELO));
         average = teamELO / len(team);
         Q = math.pow(10.0, (average / 400));
@@ -67,38 +81,48 @@ for match in matches:
         #print(" Q: " + str(Q));
         teamELOs.append(average);
         teamQs.append(Q);
+        teamNames.append(name);
     totalELO = sum(teamELOs);
     totalQ = sum(teamQs);
     #print ("Total ELO: " + str(totalELO));
     #print("Total Q:" + str(totalQ));
     index = 0;
+    winningIndex = 0;
+    winningPercent = 0;
     for eachq in teamQs:
         winchance = eachq / totalQ;
         print(str(match[index]));
         print("Chance of winning: " + str(winchance));
+        if winchance > winningPercent:
+            winningIndex = index;
+            winningPercent = winchance;
         index += 1;
     print("Average ELO for match: " + str(totalELO / len(match)));
     print("");
     
-    print("");
-    print("<p style=\"font-size:32px;font-weight:bold;text-align:center\">Matchup</p>");
-    print("<p style=\"font-size:28px;font-weight:bold;text-align:center;margin-top:0\">Title</p>");
-    print("<p style=\"font-size:24px;padding:10px;margin:0\">Elo Ratings:</p>");
-    print("<ul style=\"line-height:90%;margin:0;margin-left:40px;padding:0\">");
-    print("<li></li>");
-    print("<li><b>Average Match Elo: </b></li>");
-    print("</ul>");
-    print("<p style=\"font-size:24px;padding:10px;margin-top:40px;margin-bottom:0px\">Elo Breakdown:</p>");
-    print("<p style=\"margin-left:40px;margin-bottom:10px\">");
-    print("breakdown");
-    print("<ul style=\"line-height:90%;margin:0;margin-left:80px;margin-bottom:10px;padding:0\">");
-    print("<li></li>");
-    print("</ul>");
-    print("<strong style=\"font-size:22px;margin-top:10px\">Elo Prediction: </strong></p>");
-    print("<p style=\"font-size:24px;padding:10px;margin-top:40px;margin-bottom:0px\">Author Breakdown:</p>");
-    print("<p style=\"margin-left:40px;margin-bottom:10px\">");
-    print("<strong style=\"font-size:22px;margin-top:10px\">Author's Prediction: </strong>");
-    print("</p><hr>");  
-    print("");
-    print("");
+    print("<p style=\"font-size:32px;font-weight:bold;text-align:center\">", end="", file=f);
+    print(' vs. '.join(teamNames), end="", file=f);
+    print("</p>", file=f);
+    print("<p style=\"font-size:28px;font-weight:bold;text-align:center;margin-top:0\">Title</p>", file=f);
+    print("<p style=\"font-size:24px;padding:10px;margin:0\">Elo Ratings:</p>", file=f);
+    print("<ul style=\"line-height:90%;margin:0;margin-left:40px;padding:0\">", file=f);
+    for x in range(0, len(teamNames)):
+        print("<li>" + teamNames[x] + ": " + str(round(teamELOs[x])) + "</li>", file=f);
+    print("<li><b>Average Match Elo: " + str(round(totalELO / len(match))) + "</b></li>", file=f);
+    print("</ul>", file=f);
+    print("<p style=\"font-size:24px;padding:10px;margin-top:40px;margin-bottom:0px\">Elo Breakdown:</p>", file=f);
+    print("<p style=\"margin-left:40px;margin-bottom:10px\">", file=f);
+    print("breakdown", file=f);
+    print("<ul style=\"line-height:90%;margin:0;margin-left:80px;margin-bottom:10px;padding:0\">", file=f);
+    print("<li></li>", file=f);
+    print("</ul>", file=f);
+    print("<strong style=\"font-size:22px;margin-top:10px\">Elo Prediction: ", end="", file=f);
+    print(teamNames[winningIndex] + " (" + str(round(winningPercent * 100)) + "% chance of winning)", end="", file=f);
+    print("</strong></p>", file=f);
+    print("<p style=\"font-size:24px;padding:10px;margin-top:40px;margin-bottom:0px\">Author Breakdown:</p>", file=f);
+    print("<p style=\"margin-left:40px;margin-bottom:10px\">", file=f);
+    print("<strong style=\"font-size:22px;margin-top:10px\">Author's Prediction: </strong>", file=f);
+    print("</p><hr>", file=f);  
+    print("", file=f);
     
+f.close();
